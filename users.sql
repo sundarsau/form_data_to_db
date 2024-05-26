ff939f5265c8af67685e8bb2142c83d0d7e89a3d
sundarsau <sundarsau@gmail.com>
2024-05-26 22:19:21 +0530

    Create users.sql

---
+++ users.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `age` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
